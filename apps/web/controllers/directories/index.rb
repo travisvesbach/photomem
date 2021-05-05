54d9121083b76157db1242db87bc1d4205ee87fe
Travis Vesbach <tav1990@hotmail.com>
2021-05-04 22:07:15 -0500

    Improve information on index and directory pages, add svg icons

--- apps/web/controllers/directories/index.rb
+++ apps/web/controllers/directories/index.rb
@@ -4,10 +4,12 @@ module Web
             class Index
                 include Web::Action
 
-                expose :directories
+                expose :directories, :image_count
 
                 def call(params)
                     @directories = DirectoryRepository.new.orderedByPath.to_a
+
+                    @image_count = @images = ImageRepository.new.all.to_a.count
                 end
             end
         end
